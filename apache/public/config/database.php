36419d750f2481a0020cb6448bf5d81ce7669190
Lyonel Rosales <lyonel.rosales@visualk.com>
2025-11-19 08:24:34 -0600

    Proyecto PHP Docker para Railway

--- apache/public/config/database.php
+++ apache/public/config/database.php
@@ -1,8 +1,14 @@
 <?php
-$host = "postgres_db";
+/*$host = "postgres_db";
 $dbname = "mi_basedatos";
 $user = "admin";
-$password = "admin123";
+$password = "admin123";*/
+
+$host = getenv('PGHOST') ?: 'postgres_db';
+$port = getenv('PGPORT') ?: '5432';
+$dbname   = getenv('PGDATABASE') ?: 'mi_basedatos';
+$user = getenv('PGUSER') ?: 'admin';
+$password = getenv('PGPASSWORD') ?: 'admin123';
 
 try {
     $conexion = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
